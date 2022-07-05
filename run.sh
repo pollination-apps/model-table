5f0a76fc4b32bf6731648a8f8996c0b42c8eb60a
NGimbal <nicolas.p.schmidt@gmail.com>
2022-07-05 17:03:10 -0400

    chore(streamlit-io): upgrades pollination-streamlit-io

--- run.sh
+++ run.sh
@@ -1,3 +1,19 @@
-echo pwd
-docker build -f {docker_file} -t {owner}/{slug}:{tag} /Users/nicolasschmidt/Documents/Pollination/model-table/
-docker run -t -i --expose 8501 -p 8501:8501 -v {path}:/app {owner}/{slug}:{tag} streamlit run app.py
+while getopts o:s:t: flag
+do
+    case "${flag}" in
+        o) owner=${OPTARG};;
+        s) slug=${OPTARG};;
+        t) tag=${OPTARG};;
+    esac
+done
+
+echo "Owner: $owner"
+echo "Slug: $slug"
+echo "Tag: $tag"
+
+pwd=$(pwd)
+
+echo "Path $pwd"
+
+docker build -f $pwd/Dockerfile -t $owner/$slug:$tag $pwd
+docker run -t -i --expose 8501 -p 8501:8501 -v $pwd:/app $owner/$slug:$tag streamlit run app.py
